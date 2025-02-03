7c7cdc2b2d50c4cc1e68e27714d83d48af01b468
YongliHe23 <yonglihe@umich.edu>
2025-02-03 11:19:34 -0500

    new commit

--- recon/readnoise.m
+++ recon/readnoise.m
@@ -6,19 +6,22 @@ np = 10;
 hmriutils.epi.io.draw2hdf(D, etl, np, fn, 'maxFramesPerFile', 50);
 
 
-
+%%
 mb=6;
 clear draw dfr
+fn=[save_root 'noise.h5'];
 draw = hmriutils.epi.io.readframe(fn, 1);
 dfr = hmriutils.epi.rampsampepi2cart(draw, kxo, kxe, nx, fov(1)*100, 'nufft'); 
 dfr = hmriutils.epi.epiphasecorrect(dfr, a);    %  [nx etl np nc]
-caipi_path='/home/yonglihe/Documents/MATLAB/gre3d_IVsat/paper-ovs-compare/EPI/SSAD/mb6_beta/caipi.mat';
-d_noise=fillCaipi(dfr,caipi_path);
-I_noise=zeros(nx,ny,nz);
-I_noise=bart('pics -l1 -r0.001',d_noise,smap);
+% caipi_path='/home/yonglihe/Documents/MATLAB/gre3d_IVsat/paper-ovs-compare/EPI/SSAD/mb6_beta/caipi.mat';
+% d_noise=fillCaipi(dfr,caipi_path);
+% I_noise=zeros(nx,ny,nz);
+% I_noise=bart('pics -l1 -r0.001',d_noise,smap);
 
 
 %% mean and variance
 D=reshape(dfr,[],nc);
-mu_noise=mean(real(D),1)+1i*mean(imag(D),1); %mean of the noise; two ch's: real and imag
-cov_noise=cov(real(D))+1i*cov(imag(D)); %covariance matrix; complex
+mu_noise=mean(D,1);
+cov_noise=((D-mu_noise)'*(D-mu_noise))/(size(D,1)-1);
+% mu_noise=mean(real(D),1)+1i*mean(imag(D),1); %mean of the noise; two ch's: real and imag
+% cov_noise=cov(real(D))+1i*cov(imag(D)); %covariance matrix; complex
